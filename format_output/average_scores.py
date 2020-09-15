46b84c64f0259700ce05d4445604b4ce86275496
Levi Shepherd <levi.d.shepherd@outlook.com>
2020-09-14 23:29:46 -0500

    added function to average()

--- format_output/average_scores.py
+++ format_output/average_scores.py
@@ -1,2 +1,15 @@
+from builtins import input, float
+
+
 def average():
-    pass
+    num_of_scores = 3
+    score1 = input("Enter your first score: ")
+    score2 = input("Enter a second score: ")
+    score3 = input("Enter a third score: ")
+    average_score = (float(score1) + float(score2) + float(score3)) / num_of_scores
+
+    return average_score
+
+
+if __name__ == '__main__':
+    average_scores = average()
